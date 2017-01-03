0fc314932d00ff781973ad535bb4949d4f9b1b38
p92canca <p92canca@uco.es>
2017-01-03 17:20:51 +0100

    Creado el directorio Templates dentro de la app. Añadida vista principal, esqueleto.html, y añadido url al urls.py de la app

--- video/views.py
+++ video/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the videos index.")
+    
+def mostrarPrincipal(request):
+    r=models.Video.objects.all().filter(privacidad_video=False)
+    return render(request,'mostrarResultados.html',{'titulo':"Pagina principal", 'resultados':r})
+
